492aed1438c56796284fff15ad8e49ab07df6e9d
Chunch <NWAColec7275@gmail.com>
2024-09-16 11:48:24 -0700

    Unit2A Done

---
+++ Ccole_Unit2A/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float HorizontalInput;
+    public float Speed = 10.0f;
+    private float Xrange = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * Speed);
+
+
+        if (transform.position.x > Xrange)
+        {
+            transform.position = new Vector3(Xrange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -Xrange)
+        {
+            transform.position = new Vector3(-Xrange, transform.position.y, transform.position.z);
+        }
+
+    }
+}
