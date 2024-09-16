2bafa38e78dcd36a837cf59163151cdc41b7c31f
Chunch <NWAColec7275@gmail.com>
2024-09-16 11:53:43 -0700

    U2LB

---
+++ Ccole_Unit2B/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float TopofScene = 30.0f;
+    private float BottomofScene = -10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > TopofScene)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < BottomofScene)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
